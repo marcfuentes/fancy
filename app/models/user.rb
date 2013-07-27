4929e4aae719af88812e5808a6377563f57a9f7c
marcelo <hemakesnoise@gmail.com>
2013-07-27 14:22:14 -0400

    relationships added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   require 'role_model'
 
   has_many :posts, :dependent => :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -24,4 +30,18 @@ class User < ActiveRecord::Base
   # roles later, always append them at the end!
   roles :admin, :user
   
+
+
+def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
 end
